8b313e2b4dacecc4ad32c62369b236446462787a
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-06 00:23:36 +0600

    add promit

---
+++ setTimeOut.js
@@ -0,0 +1,11 @@
+const addition = () =>{
+    const number1=10;
+    const number2=20;
+    const sum = number1+number2
+    console.log(sum)
+    return sum;
+}
+console.log("first")
+console.log("second")
+
+setTimeout(addition,3000)
